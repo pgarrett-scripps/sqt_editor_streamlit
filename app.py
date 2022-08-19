ece3ce353c4a629c9405c4758b4e1a1b5d684cdf
pgarrett <pgarrett@scripps.edu>
2022-08-19 14:36:45 -0700

    update

--- app.py
+++ app.py
@@ -9,27 +9,32 @@ st.write("Sets all mobility / timscore values to zero")
 
 sqt_file = st.file_uploader("SQT File", ".sqt")
 
-if sqt_file:
+if st.button("Generate"):
 
-    with st.spinner("Reading SQT File"):
-        sqt_lines = sqt_file.getvalue().decode('utf-8').split('\n')
-        h_lines, s_lines = read_file(sqt_lines)
+    if not sqt_file:
+        st.error("Upload SQT file...")
 
-    for s_line in s_lines:
-        for m_line in s_line.m_lines:
-            m_line.tims_score = 0
-            m_line.predicted_ook0 = 0
+    else:
 
-    with st.spinner("Writing SQT File"):
-        lines = []
-        for h_line in h_lines:
-            lines.append(h_line.serialize(version='v2.1.0_ext'))
+        with st.spinner("Reading SQT File"):
+            sqt_lines = sqt_file.getvalue().decode('utf-8').split('\n')
+            h_lines, s_lines = read_file(sqt_lines)
+
         for s_line in s_lines:
-            lines.append(s_line.serialize(version='v2.1.0_ext'))
             for m_line in s_line.m_lines:
-                lines.append(m_line.serialize(version='v2.1.0_ext'))
-                for l_line in m_line.l_lines:
-                    lines.append(l_line.serialize(version='v2.1.0_ext'))
-        lines.append('\n')
+                m_line.tims_score = 0
+                m_line.predicted_ook0 = 0
 
-    st.download_button("Download Zero SQT", "".join(lines), file_name=f"{Path(sqt_file.name).stem}_ZERO.sqt")
+        with st.spinner("Writing SQT File"):
+            lines = []
+            for h_line in h_lines:
+                lines.append(h_line.serialize(version='v2.1.0_ext'))
+            for s_line in s_lines:
+                lines.append(s_line.serialize(version='v2.1.0_ext'))
+                for m_line in s_line.m_lines:
+                    lines.append(m_line.serialize(version='v2.1.0_ext'))
+                    for l_line in m_line.l_lines:
+                        lines.append(l_line.serialize(version='v2.1.0_ext'))
+            lines.append('\n')
+
+        st.download_button("Download Zero SQT", "".join(lines), file_name=f"{Path(sqt_file.name).stem}_ZERO.sqt")
